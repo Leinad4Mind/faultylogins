eb175dd1b514080b5493d3caafdbad2d4a7e64ec
ThE KuKa <thekuka@gmail.com>
2017-08-14 01:00:36 +0200

    Spanish translation
    Spanish translation

---
+++ language/es/info_acp_faultylogins.php
@@ -0,0 +1,22 @@
+<?php
+/**
+*
+* @package Faulty logins
+* @copyright (c) 2014 John Peskens (http://ForumHulp.com)
+* @license Proprietary
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'LOG_FAULTY_LOGINS'	=> '<strong>Error de inicio de sesión</strong><br />» %1s » %2s<br />» %3s',
+));
